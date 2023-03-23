a45b777af009478a783e366cdeecb853f8cc955f
TECHO <112702470+hightech-lab@users.noreply.github.com>
2023-03-23 13:02:49 +0530

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -5,7 +5,7 @@ let pp = await conn.profilePictureUrl(who).catch(_ => hwaifu.getRandom())
 let name = await conn.getName(who)
 
   const sentMsg = await conn.sendContactArray(m.chat, [
-    [`${nomorown}`, `${await conn.getName(nomorown+'@s.whatsapp.net')}`, `💌 Developer Bot `, `ɴᴏᴛ ғᴀᴍᴏᴜs ᴊᴜsᴛ ᴀʟᴏɴᴇ ʙᴏʏ`, `thightech22@gmail.com`, `🇮🇩 Srilanka`, `📍 https://github.com/hightech-lab`, `👤 Owner Of Queen Moonlight Bot`],
+    [`${nomorown}`, `${await conn.getName(nomorown+'@s.whatsapp.net')}`, `💌 Developer Bot `, `I am Moonlight's husband`, `thightech22@gmail.com`, `🇮🇩 Srilanka`, `📍 https://github.com/hightech-lab`, `👤 Owner Of Queen Moonlight Bot`],
     [`${conn.user.jid.split('@')[0]}`, `${await conn.getName(conn.user.jid)}`, `🎈 ʙᴏᴛ ᴡʜᴀᴛsᴀᴘᴘ`, `📵 ᴅᴏɴᴛ sᴘᴀᴍ/ᴄᴀʟʟ ᴍᴇ 😢`, `thightech22@gmail.com`, `🇮🇩 Srilanka`, `📍 https://github.com/hightech-lab/Queen-Moonlight-10.2v`, `If you have any questions about this bot, please contact the creator of this bot. ☺`]
   ], fkontak)
   await m.reply(`*ʜᴇʟʟᴏ @${m.sender.split(`@`)[0]} ᴛʜᴀᴛs ᴍʏ ᴏᴡɴᴇʀ ᴅᴏɴᴛ sᴘᴀᴍ ᴏʀ ʏᴏᴜ ᴡɪʟʟ ʙᴇ ʙʟᴏᴄᴋᴇᴅ...*`)
